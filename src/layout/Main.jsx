70a1999059d78ca0d2a6b227e7667888b0bced5f
Mihael404 <slutskij.m.a@gmail.com>
2024-02-05 14:36:40 +0300

    First commit: Base config: Header component Main component Footer component
    Materialize css + simple custom styles

---
+++ src/layout/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main(){
+    return(
+        <main className="content">
+            <div className="container">
+                Hello from Movies App
+            </div>
+        </main>
+    )
+}
